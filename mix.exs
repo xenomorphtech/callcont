f7446cd3e1b2ff6607862215c092b3ab1a278210
lissana <ktion23@gmail.com>
2025-05-03 20:56:49 +0800

    bump req elixir to 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Callcont.MixProject do
     [
       app: :callcont,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
@@ -30,7 +30,7 @@ defmodule Callcont.MixProject do
     [
       # Only needed if different from OTP application name
       name: "callcont",
-      files: ~w(lib priv .formatter.exs mix.exs README* LICENSE* CHANGELOG*),
+      files: ~w(lib .formatter.exs mix.exs README*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/xenomorphtech/callcont"}
     ]
@@ -39,6 +39,7 @@ defmodule Callcont.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
